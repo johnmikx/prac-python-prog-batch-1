31c1dc27dc8a62225333a1085e92d0d1b3dacfe2
John Mike Asuncion <johnmikeasuncion17@gmail.com>
2025-03-06 00:58:03 +0800

    Python Exercises

--- prog07.py
+++ prog07.py
@@ -3,7 +3,7 @@
 total = 0
 
 for i in range(10):
-    num = float(input(f"Enter number {i+1}: "))
+    num = float(input(f"Enter number {i + 1}: "))
     total += num
 
 print(f"The sum of all numbers is: {total}")
