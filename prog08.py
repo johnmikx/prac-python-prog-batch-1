0a3417ca9fcccae5015c54f5cb0cd31d309ab202
John Mike Asuncion <johnmikeasuncion17@gmail.com>
2025-03-06 00:57:40 +0800

    Python Exercises

--- prog08.py
+++ prog08.py
@@ -3,7 +3,7 @@
 odd_count = 0
 
 for i in range(10):
-    num = float(input(f"Enter number {i + 1}: "))
+    num = int(input(f"Enter number {i+1}: "))
 
     if num % 2 != 0:
         odd_count += 1
